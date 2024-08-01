d10c85d4563773cd3e5877c6882efdfc2ec6f9b3
Posccis <lkzq33@gmail.com>
2024-08-01 13:27:35 -0300

    update(main) = ajustes necessários na classe de usuário

--- PicPayment.Domain/Domains/Usuario.cs
+++ PicPayment.Domain/Domains/Usuario.cs
@@ -1,8 +1,10 @@
 using PicPayment.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PicPayment.Domain.Domains
@@ -10,13 +12,29 @@ namespace PicPayment.Domain.Domains
     public class Usuario : IUsuario
     {
         public Guid Id { get; set; }
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
+        [Required]
+        [MinLength(2, ErrorMessage = "O nome do cliente deve possuir pelo menos 2 caracteres.")]
+        [MaxLength(100, ErrorMessage = "O nome do cliente deve possuir no máximo 100 caracteres.")]
         public string NomeCompleto { get; set; }
+        /// <summary>
+        /// CPF do cliente
+        /// </summary>
+        [Required]
         public long CPF { get; set; }
         public string Email { get; set; }
+        /// <summary>
+        /// CPF do cliente
+        /// </summary>
+        [Required]
         public string Senha { get; set; }
         public string Categoria { get; set; }
         public double Saldo { get; set; }
+        [JsonIgnore]
         public ICollection<Transferencia> TransferenciasOrigem { get; set; }
+        [JsonIgnore]
         public ICollection<Transferencia> TransferenciasDestino { get; set; }
     }
 }
