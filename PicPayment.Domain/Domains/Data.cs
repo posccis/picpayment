6911b6b4cea005ac196a9d36746e8dd1ab548311
Posccis <lkzq33@gmail.com>
2024-08-14 13:33:25 -0300

    new(main) = Criação da classe data para receber o status de autorização do endpoint terceiro

---
+++ PicPayment.Domain/Domains/Data.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicPayment.Domain.Domains
+{
+    public class Data
+    {
+        public bool Authorization { get; set; }
+    }
+}
